f9307d9d63141c54942ac20a322af67cc6a42426
João Miguel <joao@miguel.ms>
2017-06-30 19:11:47 -0300

    Rowback

--- src/br/com/centaurotech/powershell.groovy
+++ src/br/com/centaurotech/powershell.groovy
@@ -6,19 +6,19 @@ def exec(Map map = [:], command) {
 
 	if (debug) echo "[DEBUG] powershell method called with parameter: \n $command"
 
-	// def returnFileName = new Date().format("yyyyMMddHHmmssSSS")
-	// if (debug) echo "[DEBUG] The return file name will is $returnFileName"
+	def returnFileName = new Date().format("yyyyMMddHHmmssSSS")
+	if (debug) echo "[DEBUG] The return file name will is $returnFileName"
 	
-	def pwCommand =  "powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile  -Command \"$command\""
+	def pwCommand =  "powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile  -Command \"$command\" > $returnFileName"
 	if(debug) echo "[DEBUG] The following command will be executed: \n $pwCommand"
-	return pwCommand.execute().text
+	bat pwCommand
 	
-	// if(debug) echo "[DEBUG] Reading the file $returnFileName"
-	// def pwret = readFile "$returnFileName"
-	// pwret = pwret.trim()
+	if(debug) echo "[DEBUG] Reading the file $returnFileName"
+	def pwret = readFile "$returnFileName"
+	pwret = pwret.trim()
 	
-	// if(debug) echo "[DEBUG] Deleting the file $returnFileName"
-	// bat "del $returnFileName"
+	if(debug) echo "[DEBUG] Deleting the file $returnFileName"
+	bat "del $returnFileName"
 	
-	// return pwret
+	return pwret
 }
