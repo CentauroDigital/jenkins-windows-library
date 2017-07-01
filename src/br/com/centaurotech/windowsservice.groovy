bf35194f370866d02a6ff270bbded0587235dc0a
João Miguel <joao@miguel.ms>
2017-06-30 21:12:50 -0300

    Better console output.

--- src/br/com/centaurotech/windowsservice.groovy
+++ src/br/com/centaurotech/windowsservice.groovy
@@ -5,21 +5,21 @@ def isInstalled(Map map = [:], hostName, serviceName) {
 	def supressOutput =  map.supressOutput ?: false
 	if (debug) supressOutput = false;
 
-	if (debug) echo "[DEBUG] isInstalled method called: hostName:\"$hostName\", serviceName:\"$serviceName\""
+	if (debug) echo "[jenkins-windows-library windows service] [DEBUG] isInstalled method called: hostName:\"$hostName\", serviceName:\"$serviceName\""
 	
-	if(!supressOutput) echo "Checking if the service $serviceName is installed on $hostName host."
+	if(!supressOutput) echo "[jenkins-windows-library windows service] Checking if the service $serviceName is installed on $hostName host."
 
 	def pwret = powershell returnStdout: true, script:"(get-service -ComputerName $hostName | Where-Object {\$_.name -eq \"$serviceName\"})"
 	def installed = (pwret.trim().length() > 0)
 
 	if (installed){
-		if(!supressOutput) echo "The service $serviceName is installed on $hostName host."
+		if(!supressOutput) echo "[jenkins-windows-library windows service] The service $serviceName is installed on $hostName host."
 	}else{
-		if(!supressOutput) echo "The service $serviceName is not installed on $hostName host."
+		if(!supressOutput) echo "[jenkins-windows-library windows service] The service $serviceName is not installed on $hostName host."
 	}
 
 	if (debug) {
-		if (debug) echo "[DEBUG] $serviceName is installed: $installed."
+		if (debug) echo "[jenkins-windows-library windows service] [DEBUG] $serviceName is installed: $installed."
 		if (installed) echo pwret
 	}
 
@@ -31,19 +31,19 @@ def getStatus(Map map = [:], hostName, serviceName) {
 	def supressOutput =  map.supressOutput ?: false
 	if (debug) supressOutput = false;
 
-	if (debug) echo "[DEBUG] getStatus method called: hostName:\"$hostName\", serviceName:\"$serviceName\", debug:\"$debug\""
+	if (debug) echo "[jenkins-windows-library windows service] [DEBUG] getStatus method called: hostName:\"$hostName\", serviceName:\"$serviceName\", debug:\"$debug\""
 
-	if(!supressOutput) echo "Getting the status of the service $serviceName on $hostName host."
+	if(!supressOutput) echo "[jenkins-windows-library windows service] Getting the status of the service $serviceName on $hostName host."
 
 	if(!isInstalled(hostName, serviceName, debug: debug, supressOutput: true)) error "The service $serviceName is not installed on $hostName host."
 
-	if (debug) echo "[DEBUG] Checking $serviceName status."
+	if (debug) echo "[jenkins-windows-library windows service] [DEBUG] Checking $serviceName status."
 	def pwret =  powershell returnStdout: true, script:"(get-service -ComputerName $hostName -Name $serviceName).Status"
 	pwret = pwret.trim();
 
-	if (debug) echo "[DEBUG] $serviceName is $pwret"
+	if (debug) echo "[jenkins-windows-library windows service] [DEBUG] $serviceName is $pwret"
 
-	if(!supressOutput) echo "The service $serviceName is $pwret on $hostName host."
+	if(!supressOutput) echo "[jenkins-windows-library windows service] The service $serviceName is $pwret on $hostName host."
 
 	return pwret;
 }
@@ -52,7 +52,7 @@ def start(Map map = [:], hostName, serviceName) {
 	def debug =  map.debug ?: false
 	def timeout =  map.timeout ?: 60
 
-	if (debug) echo "[DEBUG] stop method called: hostName:\"$hostName\", serviceName:\"$serviceName\", timeout:\"$timeout\", debug:\"$debug\""
+	if (debug) echo "[jenkins-windows-library windows service] [DEBUG] stop method called: hostName:\"$hostName\", serviceName:\"$serviceName\", timeout:\"$timeout\", debug:\"$debug\""
 
 	if(!isInstalled(hostName, serviceName, debug: debug, supressOutput: true)) error "The service $serviceName is not installed on $hostName host."
 
@@ -61,15 +61,15 @@ def start(Map map = [:], hostName, serviceName) {
 	def statusToStart = ["Stopped", "StopPending", "ContinuePending", "PausePending", "Paused"]
 
 	if(isStatusInArray(status, statusToStart)){
-		echo "Starting the service $serviceName on $hostName host."
+		echo "[jenkins-windows-library windows service] Starting the service $serviceName on $hostName host."
 		powershell "(get-service -ComputerName $hostName -Name $serviceName).Start()"
 	}else{
-		echo "Service $serviceName alerady running on $hostName host."
+		echo "[jenkins-windows-library windows service] Service $serviceName alerady running on $hostName host."
 		return
 	}
 
 	powershell "(get-service -ComputerName $hostName -Name $serviceName).WaitForStatus(\"Running\")"
-	echo "Service $serviceName started on $hostName host."
+	echo "[jenkins-windows-library windows service] Service $serviceName started on $hostName host."
 }
 
 def stop(Map map = [:], hostName, serviceName) {
@@ -77,7 +77,7 @@ def stop(Map map = [:], hostName, serviceName) {
 	def timeout =  map.timeout ?: 60
 	def force =  map.force ?: false
 
-	if (debug) echo "[DEBUG] stop method called: hostName:\"$hostName\", serviceName:\"$serviceName\", force:\"$force\", timeout:\"$timeout\", debug:\"$debug\""
+	if (debug) echo "[jenkins-windows-library windows service] [DEBUG] stop method called: hostName:\"$hostName\", serviceName:\"$serviceName\", force:\"$force\", timeout:\"$timeout\", debug:\"$debug\""
 
 	if(!isInstalled(hostName, serviceName, debug: debug, supressOutput: true)) error "The service $serviceName is not installed on $hostName host."
 	
@@ -86,15 +86,15 @@ def stop(Map map = [:], hostName, serviceName) {
 	def statusToStop = ["Running", "StartPending", "ContinuePending", "PausePending", "Paused"]
 
 	if(isStatusInArray(status, statusToStop)){
-		echo "Starting the service $serviceName on $hostName host."
+		echo "[jenkins-windows-library windows service] Stopping the service $serviceName on $hostName host."
 		powershell "(get-service -ComputerName $hostName -Name $serviceName).Stop()"
 	}else{
-		echo "Service $serviceName alerady stopped on $hostName host."
+		echo "[jenkins-windows-library windows service] Service $serviceName alerady stopped on $hostName host."
 		return
 	}
 
 	powershell "(get-service -ComputerName $hostName -Name $serviceName).WaitForStatus(\"Stopped\")"
-	echo "Service $serviceName stopped on $hostName host."
+	echo "[jenkins-windows-library windows service] Service $serviceName stopped on $hostName host."
 }
 
 def isStatusInArray(status, statusArray) {
