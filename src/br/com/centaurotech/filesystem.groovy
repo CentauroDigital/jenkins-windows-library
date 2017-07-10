8f6ac6f6f605aa4a0aa15f22215498ffb11958f0
alexandre.silva <alexandre.silva@centauro.com.br>
2017-07-10 12:10:40 -0300

    file exists

--- src/br/com/centaurotech/filesystem.groovy
+++ src/br/com/centaurotech/filesystem.groovy
@@ -26,11 +26,11 @@ def copy(Map map = [:], fromPath, toServer, toPath, extensions) {
 
 def fileExist(Map map = [:], path) {
     def debug =  map.debug ?: false
-    echo "[jenkins-windows-library file system] [DEBUG] file exist method called: path: $path"
+    if (debug) echo "[jenkins-windows-library file system] [DEBUG] file exist method called: path: $path"
 
-    def pwret = powershell returnStdout: true, script:"Test-Path $path"
-    echo "[jenkins-windows-library file system] [DEBUG] file exist method called: exist: $pwret"
+    def pwret = powershell "Test-Path $path"
+    if (debug) echo "[jenkins-windows-library file system] [DEBUG] file exist method called: exist: $pwret"
 
-	def exist = (pwret.trim() == "true")
+	def exist = (pwret.trim() == "True")
     return exist
 }
