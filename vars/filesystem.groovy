cdab441e2e18d5b5a2b5782db9944d45e1f3d128
alexandre.silva <alexandre.silva@centauro.com.br>
2017-07-10 12:48:05 -0300

    copy

--- vars/filesystem.groovy
+++ vars/filesystem.groovy
@@ -2,7 +2,7 @@ class filesystem implements Serializable {
     def _filesystem = new br.com.centaurotech.filesystem()
     
     def copy(Map map = [:], fromPath, toServer, toPath, extensions) {
-        _filesystem.copyFile(map, fromPath, toServer, toPath, extensions)
+        _filesystem.copy(map, fromPath, toServer, toPath, extensions)
     }
 
     def fileExist(Map map = [:], path) {
