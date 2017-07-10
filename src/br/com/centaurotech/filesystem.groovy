72c10ba676fb4e9ad4c25101c84c86b9564ebf0a
alexandre.silva <alexandre.silva@centauro.com.br>
2017-07-10 13:41:58 -0300

    println

--- src/br/com/centaurotech/filesystem.groovy
+++ src/br/com/centaurotech/filesystem.groovy
@@ -14,7 +14,7 @@ def copy(Map map = [:], fromPath, toServer, toPath, extensions) {
             extensions.each { 
                  def ext = it.replace(".", "")
 
-                 println ext
+                 println "Copy-Item $fromPath\\*.$ext -Destination \\\\$toServer\\$toPath -recurse -Force"
 
                 if (debug) echo "[jenkins-windows-library file system] [DEBUG] Execute Command: Copy-Item $fromPath\\*.$ext -Destination \\\\$toServer\\$toPath -recurse -Force"
                 powershell "Copy-Item $fromPath\\*.$ext -Destination \\\\$toServer\\$toPath -recurse -Force"
