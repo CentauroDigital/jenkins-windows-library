a4abedf146810549e8279648b00fb01b93cd7bbd
alexandre.silva <alexandre.silva@centauro.com.br>
2017-07-10 11:43:50 -0300

    file system - copy

--- src/br/com/centaurotech/filesystem.groovy
+++ src/br/com/centaurotech/filesystem.groovy
@@ -1,28 +1,29 @@
 package br.com.centaurotech
 
-def copyFile(Map map = [:], fromPath, toServer, toPath) {
+def copy(Map map = [:], fromPath, toServer, toPath, extensions) {
     def debug =  map.debug ?: false
-    if (debug) echo "[jenkins-windows-library file system] [DEBUG] copy file method called: from path: $fromPath, to server: $toServer, to path: $toPath .Command: Copy-Item $fromPath -Destination \\\\$toServer\\$toPath"
 
-    powershell "Copy-Item $fromPath -Destination \\\\$toServer\\$toPath"
+    def exist = fileExist(fromPath)
+    if (exist) {
+        if (extensions == null) {
+            if (debug) echo "[jenkins-windows-library file system] [DEBUG] copy method called: from path: $fromPath, to server: $toServer, to path: $toPath .Command: Copy-Item $fromPath -Destination \\\\$toServer\\$toPath -recurse -Force"
+            powershell "Copy-Item $fromPath -Destination \\\\$toServer\\$toPath -recurse -Force"
+        } else {
+            if (debug) echo "[jenkins-windows-library file system] [DEBUG] copy method called: from path: $fromPath, to server: $toServer, to path: $toPath, extensions: $extensions"
+            def extensionsArray = extensions.split(',').collect{ext as String}
+
+            extensionsArray.each { ext ->
+                ext = ext.replace(".", "")
+
+                if (debug) echo "[jenkins-windows-library file system] [DEBUG] Execute Command: Copy-Item $fromPath\\*.${ext} -Destination \\\\$toServer\\$toPath -recurse -Force"
+                powershell "Copy-Item $fromPath\\*.${ext} -Destination \\\\$toServer\\$toPath -recurse -Force"
+            }
+        }
+    } else {
+        if (debug) echo "[jenkins-windows-library file system] [DEBUG] copy file method called: Error - $fromPath not exist"
+    }
 }
 
-def copyFolder(Map map = [:], fromPath, toServer, toPath, extensions) {
-    def debug =  map.debug ?: false
-
-    if (extensions == null) {
-        if (debug) echo "[jenkins-windows-library file system] [DEBUG] copy folder method called: from path: $fromPath, to server: $toServer, to path: $toPath .Command: Copy-Item $fromPath -Destination \\\\$toServer\\$toPath -recurse"
-
-        powershell "Copy-Item $fromPath -Destination \\\\$toServer\\$toPath -recurse"
-    } else {
-        if (debug) echo "[jenkins-windows-library file system] [DEBUG] copy folder method called: from path: $fromPath, to server: $toServer, to path: $toPath, extensions: $extensions"
-        def extensionsArray = extensions.split(',').collect{ext as String}
-
-        extensionsArray.each { ext ->
-            ext = ext.replace(".", "")
-            if (debug) echo "[jenkins-windows-library file system] [DEBUG] Execute Command: Copy-Item $fromPath\\*.${ext} -Destination \\\\$toServer\\$toPath"
-
-            powershell "Copy-Item $fromPath\\*.${ext} -Destination \\\\$toServer\\$toPath"
-        }
-    }
+def fileExist(path) {
+    return powershell "Test-Path $path"
 }
--- vars/filesystem.groovy
+++ vars/filesystem.groovy
@@ -1,11 +1,7 @@
 class filesystem implements Serializable {
     def _filesystem = new br.com.centaurotech.filesystem()
     
-    def copyFile(Map map = [:], fromPath, toServer, toPath) {
-        _filesystem.copyFile(map, fromPath, toServer, toPath)
-    }
-
-    def copyFolder(Map map = [:], fromPath, toServer, toPath, extensions) {
-         _filesystem.copyFolder(map, fromPath, toServer, toPath, extensions)
+    def copy(Map map = [:], fromPath, toServer, toPath, extensions) {
+        _filesystem.copyFile(map, fromPath, toServer, toPath, extensions)
     }
 }
