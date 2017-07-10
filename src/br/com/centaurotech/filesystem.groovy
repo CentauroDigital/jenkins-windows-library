45030a62485f03173db5225dc8320c4f4c70044f
alexandre.silva <alexandre.silva@centauro.com.br>
2017-07-10 13:39:41 -0300

    it

--- src/br/com/centaurotech/filesystem.groovy
+++ src/br/com/centaurotech/filesystem.groovy
@@ -6,29 +6,29 @@ def copy(Map map = [:], fromPath, toServer, toPath, extensions) {
     def exist = fileExist(map, fromPath)
     if (exist) {
         if (extensions == null) {
-            if (debug) println "[jenkins-windows-library file system] [DEBUG] copy method called: from path: $fromPath, to server: $toServer, to path: $toPath .Command: Copy-Item $fromPath -Destination \\\\$toServer\\$toPath -recurse -Force"
+            if (debug) echo "[jenkins-windows-library file system] [DEBUG] copy method called: from path: $fromPath, to server: $toServer, to path: $toPath .Command: Copy-Item $fromPath -Destination \\\\$toServer\\$toPath -recurse -Force"
             powershell "Copy-Item $fromPath -Destination \\\\$toServer\\$toPath -recurse -Force"
         } else {
-            if (debug) println "[jenkins-windows-library file system] [DEBUG] copy method called: from path: $fromPath, to server: $toServer, to path: $toPath, extensions: $extensions"
+            if (debug) echo "[jenkins-windows-library file system] [DEBUG] copy method called: from path: $fromPath, to server: $toServer, to path: $toPath, extensions: $extensions"
 
-            extensions.each { item ->
-                def ext = item.replace(".", "")
+            extensions.each { 
+                 def ext = it.replace(".", "")
 
-                if (debug) println "[jenkins-windows-library file system] [DEBUG] Execute Command: Copy-Item $fromPath\\*.$ext -Destination \\\\$toServer\\$toPath -recurse -Force"
+                if (debug) echo "[jenkins-windows-library file system] [DEBUG] Execute Command: Copy-Item $fromPath\\*.$ext -Destination \\\\$toServer\\$toPath -recurse -Force"
                 powershell "Copy-Item $fromPath\\*.$ext -Destination \\\\$toServer\\$toPath -recurse -Force"
             }
         }
     } else {
-        if (debug) println "[jenkins-windows-library file system] [DEBUG] copy file method called: Error - $fromPath not exist"
+        if (debug) echo "[jenkins-windows-library file system] [DEBUG] copy file method called: Error - $fromPath not exist"
     }
 }
 
 def fileExist(Map map = [:], path) {
     def debug =  map.debug ?: false
-    if (debug) println "[jenkins-windows-library file system] [DEBUG] file exist method called: path: $path"
+    if (debug) echo "[jenkins-windows-library file system] [DEBUG] file exist method called: path: $path"
 
     def pwret = powershell returnStdout: true, script:"Test-Path $path"
-    if (debug) println "[jenkins-windows-library file system] [DEBUG] file exist method called: exist: $pwret"
+    if (debug) echo "[jenkins-windows-library file system] [DEBUG] file exist method called: exist: $pwret"
 
 	def exist =  false
     if (pwret != null) {
