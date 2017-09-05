5851809b7690202f76b345602d36eedee370ed84
218735 <laura.lima@centauro.com.br>
2017-09-05 08:59:48 -0300

    Correção do método delete

--- src/br/com/centaurotech/filesystem.groovy
+++ src/br/com/centaurotech/filesystem.groovy
@@ -31,6 +31,8 @@ def fileExist(Map map = [:], path) {
 def delete(Map map = [:], path){
     def debug =  map.debug ?: false
 
+    
+
     def exist = fileExist(map, path)
     if (debug) echo "[jenkins-windows-library file system] [DEBUG] fileExist method called: exist: $exist."
     if (exist) {
