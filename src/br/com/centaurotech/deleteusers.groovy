f53857789cadd7f2939a98c68057db50ae2a3d15
218735 <laura.lima@centauro.com.br>
2017-09-06 12:08:59 -0300

    "correções"

--- src/br/com/centaurotech/deleteusers.groovy
+++ src/br/com/centaurotech/deleteusers.groovy
@@ -1,7 +1,3 @@
-def delete(Map map = [:], server){
-
-    def exist = fileExist(map, path)
-    if (exist) {
-        powershell "Invoke-Command -ComputerName $server -ScriptBlock  {Get-WMIObject -class Win32_UserProfile | Where {(!$_.Special) -and ($_.ConvertToDateTime($_.LastUseTime) -lt (Get-Date).AddDays(-5))} | Remove-WmiObject}"
-    }
+def delete(Map map = [:], server) {
+    powershell "Invoke-Command -ComputerName $server -ScriptBlock  {Get-WMIObject -class Win32_UserProfile | Where {(!$_.Special) -and ($_.ConvertToDateTime($_.LastUseTime) -lt (Get-Date).AddDays(-5))} | Remove-WmiObject}"
 }
