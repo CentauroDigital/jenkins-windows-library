a1a62562b3b0d6000f7ea9d67bae789b8119dcd1
218735 <laura.lima@centauro.com.br>
2017-09-06 12:16:59 -0300

    debug

--- vars/deleteusers.groovy
+++ vars/deleteusers.groovy
@@ -2,6 +2,11 @@ class deleteusers implements Serializable {
     def _deleteusers = new br.com.centaurotech.deleteusers()
     
     def delete(Map map = [:], server) {
+        def debug =  map.debug ?: false
+        if (debug) {
+            echo "call class deleteusers"
+        }
+
         _deleteusers.delete(map, server)
     }
 }
