cc72d4b963980023668222fc0e431cef64571775
218735 <laura.lima@centauro.com.br>
2017-09-06 11:34:19 -0300

    correção de erros

--- jenkins delete/Apagar usuarios.groovy
+++ jenkins delete/Apagar usuarios.groovy
@@ -1,8 +1,8 @@
 def delete(Map map = [:], path){
     def debug =  map.debug ?: false
+    if (debug) echo "[jenkins-windows-library file system] [DEBUG] delete method called: from path: $path."
 
     def exist = fileExist(map, path)
     if (exist) {
         powershell "Invoke-Command -ComputerName $server -ScriptBlock  {Get-WMIObject -class Win32_UserProfile | Where {(!$_.Special) -and ($_.ConvertToDateTime($_.LastUseTime) -lt (Get-Date).AddDays(-5))} | Remove-WmiObject}"
     }
-} 
