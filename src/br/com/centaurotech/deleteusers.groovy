39b7392e4f257a56ecf98cbc50a7c0a0aede0432
218735 <laura.lima@centauro.com.br>
2017-09-06 12:13:26 -0300

    incluir package

--- src/br/com/centaurotech/deleteusers.groovy
+++ src/br/com/centaurotech/deleteusers.groovy
@@ -1,3 +1,5 @@
+package br.com.centaurotech
+
 def delete(Map map = [:], server) {
     powershell "Invoke-Command -ComputerName $server -ScriptBlock  {Get-WMIObject -class Win32_UserProfile | Where {(!$_.Special) -and ($_.ConvertToDateTime($_.LastUseTime) -lt (Get-Date).AddDays(-5))} | Remove-WmiObject}"
 }
