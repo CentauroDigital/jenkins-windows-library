d1fdd3b4096b1f69bbfe38b449288b8d0ae19182
Carlysson Andrey de Oliveira <218736@netmaster.intra>
2017-09-07 15:02:54 -0300

    removendo arquivo deleteusers da master

--- vars/deleteusers.groovy
+++
@@ -1,8 +0,0 @@
-class deleteusers implements Serializable {
-    def _deleteusers = new br.com.centaurotech.deleteusers()
-    
-    def delete(Map map = [:], server) {
-        def debug =  map.debug ?: false
-        _deleteusers.delete(map, server)
-    }
-}
