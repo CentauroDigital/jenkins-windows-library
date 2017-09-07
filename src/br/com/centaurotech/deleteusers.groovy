1b5e91682d1786dd391effb2e132c565b8b5ca2d
Carlysson Andrey de Oliveira <218736@netmaster.intra>
2017-09-07 15:34:44 -0300

    Teste concatenação

--- src/br/com/centaurotech/deleteusers.groovy
+++ src/br/com/centaurotech/deleteusers.groovy
@@ -3,8 +3,9 @@ package br.com.centaurotech
 def delete(Map map = [:], server) {
     def debug =  map.debug ?: false
     if (debug) echo  "call class deleteusers. Server: $server"
-    def script = 'Get-WMIObject -class Win32_UserProfile | Where {(!$_.Special) -and ($_.ConvertToDateTime($_.LastUseTime) -lt (Get-Date).AddDays(-5))} | Remove-WmiObject'
-    echo "Invoke-Command -ComputerName \"$server\" -ScriptBlock  {$script}"
-    powershell "Invoke-Command -ComputerName \"$server\" -ScriptBlock  {$script}"
+    def script = ''
+    
+    
+    powershell "Invoke-Command -ComputerName \"$server\" -ScriptBlock  {Get-WMIObject -class Win32_UserProfile | Where {(!$" + "_.Special) -and ($" + "_.ConvertToDateTime($" + "_.LastUseTime) -lt (Get-Date).AddDays(-5))} | Remove-WmiObject}"
 
 }
