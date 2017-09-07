8c725c226802de821b8c5dbac2e391d7852f9cd6
Carlysson Andrey de Oliveira <218736@netmaster.intra>
2017-09-07 15:01:13 -0300

    Teste criando variável script

--- src/br/com/centaurotech/deleteusers.groovy
+++ src/br/com/centaurotech/deleteusers.groovy
@@ -3,6 +3,7 @@ package br.com.centaurotech
 def delete(Map map = [:], server) {
     def debug =  map.debug ?: false
     if (debug) echo  "call class deleteusers. Server: $server"
+    def script = 'Get-WMIObject -class Win32_UserProfile | Where {(!$_.Special) -and ($_.ConvertToDateTime($_.LastUseTime) -lt (Get-Date).AddDays(-5))} | Remove-WmiObject'
     
-powershell 'Invoke-Command -ComputerName \'$server\' -ScriptBlock  {Get-WMIObject -class Win32_UserProfile | Where {(!$_.Special) -and ($_.ConvertToDateTime($_.LastUseTime) -lt (Get-Date).AddDays(-5))} | Remove-WmiObject}'
+powershell "Invoke-Command -ComputerName \"$server\" -ScriptBlock  {\"$script\"}"
 }
